e098e9ee05f39a586784ac1b8539066377c14832
JokerW <631695855@qq.com>
2015-11-20 18:01:12 +0800

    create project

---
+++ app/src/main/java/com/example/joker/viewpractice/activity/RecyclerViewActivity.java
@@ -0,0 +1,12 @@
+package com.example.joker.viewpractice.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xiufengwang on 2015/11/20.
+ */
+public class RecyclerViewActivity extends AppCompatActivity {
+
+
+
+}
---
+++ app/src/main/java/com/example/joker/viewpractice/adapter/RecyclerAdapter.java
@@ -0,0 +1,22 @@
+package com.example.joker.viewpractice.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
+/**
+ * Created by xiufengwang on 2015/11/20.
+ */
+public class RecyclerAdapter extends RecyclerView{
+    public RecyclerAdapter(Context context) {
+        super(context);
+    }
+
+    public RecyclerAdapter(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RecyclerAdapter(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
